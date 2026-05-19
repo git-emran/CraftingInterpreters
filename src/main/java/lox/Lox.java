9c3f953763815adf63bbe62ac3d7771e9e2f5898
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-05-19 21:29:16 +0600

    lexical errors

--- src/main/java/lox/Lox.java
+++ src/main/java/lox/Lox.java
@@ -1,4 +1,5 @@
-package com.craftinginterpreters.lox;
+package lox;
+
 
 import java.io.BufferedReader;
 import java.io.IOException;
---
+++ src/main/java/lox/Scanner.java
@@ -0,0 +1,88 @@
+package lox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.sun.tools.example.debug.expr.Token;
+
+import static lox.TokenType.*;
+
+
+class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    Scanner(String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens(){
+        while (!isAtEnd()) {
+            start = current;
+            scanToken();
+        }
+    }
+
+    private boolean isAtEnd(){
+        return current >= source.length();
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(':
+                addToken(LEFT_PAREN);
+                break;
+            case ')':
+                addToken(RIGHT_PAREN);
+                break;
+            case '{':
+                addToken(LEFT_BRACE);
+                break;
+            case '}':
+                addToken(RIGHT_BRACE);
+                break;
+            case ',':
+                addToken(COMMA);
+                break;
+            case '.':
+                addToken(DOT);
+                break;
+            case '-':
+                addToken(MINUS);
+                break;
+            case '+':
+                addToken(PLUS);
+                break;
+            case ';':
+                addToken(SEMICOLON);
+                break;
+            case '*':
+                addToken(STAR);
+                break;
+
+            default:
+                Lox.error(line, "Unexpected character.");
+                break;
+        }
+    }
+
+    private char advance() {
+        current++;
+        return source.charAt(current -1);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+}
